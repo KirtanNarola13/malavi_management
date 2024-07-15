f93209e95c5f66616eef4b8f6703ed4e719a0a91
kirtan <kirtannarola1209@gmail.com>
2024-07-14 23:23:03 -0700

    login and authantication

---
+++ lib/modules/screens/nav-bar-screen/nav_bar_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_snake_navigationbar/flutter_snake_navigationbar.dart';
+
+import '../bill-screen/bill_screen.dart';
+import '../home-screen/home_screen.dart';
+import '../product-screen/product_screen.dart';
+import '../profile-screen/profile_screen.dart';
+
+class NavBarScreen extends StatefulWidget {
+  const NavBarScreen({super.key});
+
+  @override
+  State<NavBarScreen> createState() => _NavBarScreenState();
+}
+
+class _NavBarScreenState extends State<NavBarScreen> {
+  int currentIndex = 0;
+
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const ProductScreen(),
+    const BillScreen(),
+    const ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[currentIndex], // Display the current screen here
+      bottomNavigationBar: SnakeNavigationBar.color(
+        behaviour: SnakeBarBehaviour.floating,
+        snakeShape: SnakeShape.circle,
+        padding: const EdgeInsets.all(12),
+        snakeViewColor: Colors.yellow,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black45,
+        showUnselectedLabels: true,
+        currentIndex: currentIndex,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add_box_outlined), label: 'Product'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_balance_wallet), label: 'Bill'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+      ),
+    );
+  }
+}
